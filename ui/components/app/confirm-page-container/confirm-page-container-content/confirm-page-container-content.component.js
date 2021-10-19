cd3cfb3e8a4ad83644cd8206d7aaf191c3b28781
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-19 15:58:55 +0100

    Fix Proptypes     Should be func not bool More Details should not have ellipsis Use a single somethingWrong i18n key. Drop Oops! from the message
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/components/app/confirm-page-container/confirm-page-container-content/confirm-page-container-content.component.js
+++ ui/components/app/confirm-page-container/confirm-page-container-content/confirm-page-container-content.component.js
@@ -148,7 +148,7 @@ export default class ConfirmPageContainerContent extends Component {
         {!EIP_1559_V2 && !hasSimulationError && (errorKey || errorMessage) && (
           <div className="confirm-page-container-content__error-container">
             <ErrorMessage
-              errorMessage={this.context.t('somethingWentWrong2')}
+              errorMessage={this.context.t('somethingWentWrong')}
               errorKey={errorKey}
               linkText={this.context.t('moreDetails')}
               onErrorMessageClick={() => onErrorMessageClick(errorMessage)}
